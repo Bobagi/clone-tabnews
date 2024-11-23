a995779b073faaf4f28505a9def14bb64ff853fe
Gustavo Perin <gustavoperin067@gmail.com>
2024-11-23 21:26:18 +0000

    feat: add audio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
-function Home(){
-    return <img src="https://i.pinimg.com/control2/736x/b0/11/32/b011327fe0dcbc64035f4314d6b772d1.jpg"></img>
+import { useRef, useEffect } from "react";
+
+function Home() {
+    const audioRef = useRef(null);
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.play().catch((err) => {
+                console.error("Erro ao tocar o áudio automaticamente:", err);
+            });
+        }
+    }, []);
+
+    const playAudio = () => {
+        if (audioRef.current) {
+            audioRef.current.play().catch((err) => {
+                console.error("Erro ao tocar o áudio:", err);
+            });
+        }
+    };
+
+    return (
+        <div>
+            <img 
+                src="https://i.pinimg.com/control2/736x/b0/11/32/b011327fe0dcbc64035f4314d6b772d1.jpg" 
+                alt="Descrição da imagem" 
+            />
+            <button onClick={playAudio}>Tocar Som</button>
+            <audio ref={audioRef} src="/oi-meu-chapa.mp3">
+                Seu navegador não suporta a reprodução de áudio.
+            </audio>
+        </div>
+    );
 }
 
 export default Home;
