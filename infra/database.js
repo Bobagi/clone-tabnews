cc5ab5b1c91d18fd02731a40f55740893e2bdf86
Gustavo Perin <gustavoperin067@gmail.com>
2024-11-24 18:55:46 +0000

    chore: error handling

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,18 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
+
+export default Object.freeze({
+  query,
+});
